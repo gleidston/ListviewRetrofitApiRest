04df58099c18ece02c6d144d2c2b1c1db993fbef
Gleidston <king2010-ro@hotmail.com>
2019-10-03 00:38:43 -0300

    versao 1 api clubes/ jogadores

---
+++ app/src/main/java/store/www/listajogadores/ui/activities/Galery.kt
@@ -0,0 +1,13 @@
+package store.www.listajogadores.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import store.www.listajogadores.R
+
+class Galery : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galery)
+    }
+}
